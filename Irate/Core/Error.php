130399923d27117f799bf5d818bacb7881137748
Matt Grubb <mgrubb0507@gmail.com>
2020-02-22 11:34:49 -0500

    Fix error log path

--- Irate/Core/Error.php
+++ Irate/Core/Error.php
@@ -27,7 +27,7 @@ class Error
             echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
             echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
         } else {
-            $log = IRATE_PATH . '/../Logs/' . date('Y-m-d') . '.txt';
+            $log = ROOT_PATH . '/Logs/' . date('Y-m-d') . '.txt';
             ini_set('error_log', $log);
 
             $message = "Uncaught exception: '" . get_class($exception) . "'";
